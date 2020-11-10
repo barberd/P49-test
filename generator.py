6a275f2255fdf17b4f5a3ab9712a28b2a8c0fc57
EC2 Default User <ec2-user@ip-172-31-79-97.ec2.internal>
2020-11-10 11:08:31 +0000

    Added better exception handling to generator

--- generator.py
+++ generator.py
@@ -8,13 +8,32 @@ import os
 import base64
 import time
 import concurrent.futures
+import math
+import botocore
 
 tablename='cerberus-test2'
 ddb=boto3.client('dynamodb')
 
+retryerrors=['ProvisionedThroughputExceededException','ThrottlingException','RequestLimitExceeded']
+
+def do_batch_write_call(RequestItems):
+    retries=0
+    while True:
+        try:
+            response=ddb.batch_write_item(RequestItems=RequestItems)
+            return response
+        except botocore.exceptions.ClientError as err:
+            if err.response['Error']['Code'] not in ['ProvisionedThroughputExceededException','ThrottlingException','RequestLimitExceeded']:
+                raise
+            print("Got Error:",err," retrying...")
+            time.sleep(math.ceil(2 ** retries,60)+random.randint(-5,5))
+            retries +=1
+            if retries>10:
+                raise
+
 def processbatch(items):
-    request=[{'PutRequest':{'Item':i}} for i in items]
-    response=ddb.batch_write_item(RequestItems={tablename:request})
+    request={tablename:[{'PutRequest':{'Item':i}} for i in items]}
+    response=do_batch_write_call(RequestItems=request)
     i = 0
     while 'UnprocessedItems' in response and response['UnprocessedItems']!={}:
         request=response['UnprocessedItems']
@@ -24,33 +43,36 @@ def processbatch(items):
     return
 
 def generate(start,end):
-    print("Starting batch",start,end)
-    items=[]
-    for i in range(start,end):
-        hexnumber="%06x"%(i)
-        firstdir=hexnumber[0:2]
-        seconddir=hexnumber[2:4]
-        pathlib.Path("data/%s/%s"%(firstdir,seconddir)).mkdir(parents=True, exist_ok=True)
-        accountid = '%040x' % random.randrange(16**40)
-        plasticid = '%010i' % random.randrange(10**10)
-        with open("data/%s/%s/%s"%(firstdir,seconddir,hexnumber),"w") as fh:
-            fh.write(accountid+","+plasticid)
-        #print(i,hexnumber,firstdir,seconddir,accountid,plasticid)
-        length=random.randint(200,400000)
-        #payload=base64.b64encode(os.urandom(length))
-        payload=os.urandom(length)
-        item={'accountDataLookupKey':{'S':accountid},
-                'sortQualifier':{'S':"sortQualifier=plasticId#"+plasticid},
-                'part-count':{'N':'001'},
-                'async-aggregates-plastic':{'B':payload}}
-        items.append(item)
-        if len(items)>24:
+    try:
+        print("Starting batch",start,end)
+        items=[]
+        for i in range(start,end):
+            hexnumber="%06x"%(i)
+            firstdir=hexnumber[0:2]
+            seconddir=hexnumber[2:4]
+            pathlib.Path("data/%s/%s"%(firstdir,seconddir)).mkdir(parents=True, exist_ok=True)
+            accountid = '%040x' % random.randrange(16**40)
+            plasticid = '%010i' % random.randrange(10**10)
+            with open("data/%s/%s/%s"%(firstdir,seconddir,hexnumber),"w") as fh:
+                fh.write(accountid+","+plasticid)
+            #print(i,hexnumber,firstdir,seconddir,accountid,plasticid)
+            length=random.randint(200,400000)
+            #payload=base64.b64encode(os.urandom(length))
+            payload=os.urandom(length)
+            item={'accountDataLookupKey':{'S':accountid},
+                    'sortQualifier':{'S':"sortQualifier=plasticId#"+plasticid},
+                    'part-count':{'N':'001'},
+                    'async-aggregates-plastic':{'B':payload}}
+            items.append(item)
+            if len(items)>24:
+                processbatch(items)
+                items=[]
+        if len(items)>0:
             processbatch(items)
-            items=[]
-    if len(items)>0:
-        processbatch(items)
-    print("Ending batch",start,end)
-
+        print("Ending batch",start,end)
+    except Exception as err:
+        print("Error in thread %i-%i:"%(start,end),err)
+        raise
 
 batch=1000
 with concurrent.futures.ThreadPoolExecutor() as executor:
